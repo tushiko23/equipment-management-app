88a7c508c4a88760a15cb8a19269d57ad2066f0c
tushima <148033059+tushiko23@users.noreply.github.com>
2026-02-08 16:36:13 +0900

    ユーザーの一覧・編集画面の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def user_after_sing_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def user_after_sing_out_path_for(resource)
+    root_path
+  end
 end
---
+++ config/importmap.rb
@@ -0,0 +1,6 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.23
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.23
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.1.200
