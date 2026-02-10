d9f8fabd4a5516f1d6e5fac84782e5b6c199ea46
tushima <148033059+tushiko23@users.noreply.github.com>
2026-02-10 21:52:10 +0000

    更新後の遷移をアカウント情報一覧に飛ばす

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [ :update ]
 
   # GET /resource/sign_up
   # def new
@@ -64,6 +64,10 @@ end
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+  end
+
+  def after_update_path_for(resource)
+    user_path(resource)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
