bf6e28fb0b12a57f00feed2821864ae2d3ce19ed
tushima <148033059+tushiko23@users.noreply.github.com>
2026-02-14 18:02:15 +0900

    Itemモデルに各モデルの関係性とバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :category
+
+  has_many :lendings, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :item_tags, dependent: :destroy
+  has_many :tags, through: :item_tags
+
+  enum state: { available: 0, borrowed: 1, maintenance: 2 }
+  validates :name, presence: true
+  validates :unique_id, presence: true, uniqueness: true
 end
