62116e3c0be57fbf9729bb7aa1d80d736f021594
tushima <148033059+tushiko23@users.noreply.github.com>
2026-02-14 18:14:55 +0900

    アイテム一覧表示とアイテムごとの詳細ページを表示するためのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :users, only: [ :show ]
+
+  resources :items, only: [ :index, :show ] do
+    resources :lendings, only: [:create]
+  end
+
+  # 【追加】マイページへのルート
+  resources :lendings, only: [ :index ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
