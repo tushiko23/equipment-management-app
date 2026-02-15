ba637259b49ef8b26daa1601e1a21959ee013651
tushima <148033059+tushiko23@users.noreply.github.com>
2026-02-15 10:21:33 +0900

    管理者用ユーザがログインしてユーザ一覧・新規作成ができるようコントローラを作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,36 @@
+class Admin::UsersController < Admin::BaseController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザーが新規作成されました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :name, :role)
+    end
+end
