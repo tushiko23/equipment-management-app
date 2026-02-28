7d4ff0aff97cd01fa7bfe6ee679dfbbca3d8953e
tushima <148033059+tushiko23@users.noreply.github.com>
2026-03-01 01:27:08 +0900

    通知テーブルに通知されたユーザーの返信に対して対応済みのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_19_155751) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_26_210001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_19_155751) do
     t.boolean "read", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "reply_message"
+    t.boolean "checked", default: false, null: false
     t.index ["item_id"], name: "index_notifications_on_item_id"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
