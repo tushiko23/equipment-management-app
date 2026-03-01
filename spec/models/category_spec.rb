dc2f1a33309b164500404950d5064e74eaf81b69
tushima <148033059+tushiko23@users.noreply.github.com>
2026-03-01 18:07:45 +0900

    カテゴリーのモデルのバリデーションテストを作成

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+RSpec.describe Category, type: :model do
+  describe 'バリデーションのテスト' do
+    context 'カテゴリー名が正しく入力されている場合' do
+      it '有効（バリデーション成功）になり、保存できること' do
+        category = Category.new(name: "PC周辺機器")
+        expect(category).to be_valid
+      end
+    end
+
+    context 'カテゴリー名が空っぽの場合' do
+      it '無効（バリデーション失敗）になり、保存できないこと' do
+        category = Category.new(name: "")
+        expect(category).to_not be_valid
+      end
+    end
+
+    context 'すでに同じ名前のカテゴリーが存在する場合' do
+      it '無効（バリデーション失敗）になり、保存できないこと' do
+        Category.create(name: "PC周辺機器")
+        category = Category.new(name: "PC周辺機器")
+        expect(category).to_not be_valid
+        category.valid?
+        expect(category.errors[:name]).to include("はすでに存在します")
+      end
+    end
+  end
+end
