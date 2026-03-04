1d1efa010a03b35ea54be1cd98eb8ff85706dbbc
tushima <148033059+tushiko23@users.noreply.github.com>
2026-03-04 17:37:04 +0900

    コメント機能のモデルスペックを作成

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let!(:random_password) { (('a'..'z').to_a.sample(8) + ('0'..'9').to_a.sample(2)).shuffle.join }
+  let!(:category) { Category.create!(name: "PC周辺機器") }
+  let!(:user) { User.create!(email: "test@example.com", password: "password123", name: "テスト太郎") }
+  let!(:item) { Item.create!(name: "MacBook", unique_id: "999", category: category, state: :available) }
+  describe "バリデーションの確認" do
+    context "異常系" do
+      it "コメントの中身が空の場合、保存されず無効(invalid)になること" do
+        comment = Comment.new(content: "", user: user, item: item)
+        expect(comment).to be_invalid！
+        expect(comment.errors.full_messages).to include("コメントを入力してください")
+      end
+    end
+  end
+end
